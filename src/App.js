c06af3e5500e44d88bf8a81c09c3472080553e90
Abrar <abrar@Abrars-MacBook-Pro.local>
2021-02-16 16:31:44 +0300

    Done

--- src/App.js
+++ src/App.js
@@ -1,22 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+// import back from "./imgs/back.jpg";
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Bakery App</h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Happiness is the smell of freshly baked BREAD!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+
       </header>
     </div>
   );
